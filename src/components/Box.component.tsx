f06fa5a080143bd47d18fa233f5d4864d19e0f39
vishnukumar <vishnu.kumar@digio.in>
2023-12-17 09:27:45 +0530

    small ui changes

--- src/components/Box.component.tsx
+++ src/components/Box.component.tsx
@@ -4,7 +4,7 @@ export default function Box({ children }: { children: React.ReactNode }) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
-    <div className="relative bg-slate-200 rounded-md shadow-3xl p-4 max-h-screen overflow-scroll">
+    <div className="relative bg-slate-200 rounded-md shadow-3xl p-4 overflow-scroll">
       <button
         className="sticky top-2 left-full h-8 aspect-square transition-all rounded-full flex items-center justify-center border-none bg-gray-900 hover:bg-gray-600 text-xl font-bold cursor-pointer z-10"
         onClick={() => setIsOpen((open) => !open)}
