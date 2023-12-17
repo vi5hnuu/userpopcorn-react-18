e087a39ad3c9cc0fed97f43db644b0d54a699861
vishnukumar <vishnu.kumar@digio.in>
2023-12-17 14:51:49 +0530

    uichanges done

--- src/components/Movie/Movie.component.tsx
+++ src/components/Movie/Movie.component.tsx
@@ -11,19 +11,24 @@ export default function Movie({
   return (
     <li
       onClick={() => onSelectMovie(movie.imdbID)}
-      className="cursor-pointer flex flex-col hover:scale-[1.02] rounded-t-sm overflow-hidden transition-all"
+      className="cursor-pointer flex flex-col p-1 rounded-md border border-slate-300 hover:scale-[1.02] hover:border-primary h-60 lg:h-96 rounded-t-sm overflow-hidden transition-all"
     >
-      <div className="flex-1">
-        <img
-          src={movie.Poster}
-          alt={`${movie.Title} poster`}
-          className="block w-full text-xl text-black h-[88%] md:h-96 lg:h-96 object-fill origin-top-left aspect-auto bg-slate-50"
-        />
-        <h3 className="text-white px-2 py-1 h-[10%] line-clamp-1 rounded-b-md bg-slate-500" title={movie.Title}>
+      <div className="flex-1 flex flex-col">
+        <div className="flex-1">
+          <img
+            src={movie.Poster}
+            alt={`${movie.Title} poster`}
+            className="block text-xl rounded-t-sm text-black h-44 lg:h-80 w-full bg-slate-50"
+          />
+        </div>
+        <h3
+          className="text-white h-8 line-clamp-1 px-2 py-1 w-full text-ellipsis rounded-b-md bg-slate-500"
+          title={movie.Title}
+        >
           {movie.Title}
         </h3>
       </div>
-      <div className="flex gap-2 text-black">
+      <div className="flex items-center h-8 gap-2 text-black">
         <span>🗓</span>
         <span>{movie.Year}</span>
       </div>
